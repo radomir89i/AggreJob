90d21ae387c5c484abd4f54fa4f1be8203cb3c34
rostislav-g <rippastiy@gmail.com>
2020-08-12 00:59:07 +0300

    changed flask project structure

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from flask import request
+import pandas as pd
+from app import app
+import os
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/get_data')
+def get_data():
+    vacancy_name = request.args.get('vacancy_name')
+    # return os.getcwd()
+    if vacancy_name == 'python':
+        content = ''
+        data = pd.read_csv('proper_data.csv', delimiter=',').values
+        for row in data:
+            content += str(list(row)) + '<br>'
+        return content
+
+    elif vacancy_name is not None and vacancy_name != 'python':
+        return f'Has no data for vacancy_name={vacancy_name}'
+    else:
+        return 'Got no "vacancy_name" parameter'
