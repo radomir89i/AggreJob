ced4a41500041824f343df947e32c1a0bde6ab5b
Radomir Vasilev <radomir89i@gmail.com>
2020-08-27 01:57:19 +0200

    Added yaml to requirements; Modified parsers

--- app/parsers.py
+++ app/parsers.py
@@ -1,80 +1,41 @@
 from abc import ABC, abstractmethod
 import requests
 import re
+import yaml
 
 
 class Parser(ABC):
+    @staticmethod
+    def get_config(parameter):
+        """Get parameter from config.yml file"""
+
+        with open('config.yml') as f:
+            config = yaml.safe_load(f)
+        return config[parameter]
+
     @abstractmethod
-    def __init__(self, main_skill, *key_skills):
-        self.main_skill = main_skill
-        self.key_skills = key_skills
+    def __init__(self, specialization):
+        self.specialization = specialization
 
     @abstractmethod
     def parse(self):
         pass
 
-    @staticmethod
-    def clean_vacancy_data(vacancy):
-        vacancy_data = {
-            'name': vacancy['name'],
-            'area': vacancy['area']['name'],
-        }
-
-        if vacancy_data.get('salary'):
-            vacancy_data['salary_from'] = vacancy['salary'].get('from')
-            vacancy_data['salary_from'] = vacancy['salary'].get('from')
-            vacancy_data['salary_currency'] = vacancy['salary']['currency']
-            vacancy_data['salary_gross'] = vacancy['salary']['gross']
-
-        vacancy_skill_set = set()
-
-        words = re.split(r'[;,"\(\)\{\}/\s]', str(vacancy))
-        words =[word.lower() for word in words]
-        for skill in Parser.KEY_SKILLS:
-            if skill in words:
-                vacancy_skill_set.add(skill)
-        vacancy_data['skill_set'] = vacancy_skill_set
-
-        return vacancy_data
+    @abstractmethod
+    def clean_vacancy_data(self, vacancy):
+        pass
 
     @staticmethod
     def write_csv_file(path, data):
         pass
 
-    KEY_SKILLS = {
-        'python',
-        'django',
-        'git',
-        'postgresql',
-        'linux',
-        'sql',
-        'docker',
-        'mysql',
-        'rest',
-        'r',
-        'javascript',
-        'html',
-        'ms sql',
-        'angularjs',
-        'atlassian jira',
-        'mongodb',
-        'c++',
-        'css',
-        'nginx',
-        'redis',
-        'react',
-        'pytorch',
-        'flask',
-        'bash',
-        'php',
-    }
-
 
 class HHParser(Parser):
-    def __init__(self, main_skill, *key_skills):
-        Parser.__init__(self, main_skill, *key_skills)
-        self.PARAMS = {
-            'text': self.main_skill,
+    def __init__(self, specialization):
+        Parser.__init__(self, specialization)
+        self.KEY_SKILLS = Parser.get_config('KEY_SKILLS')
+        self.params = {
+            'text': self.specialization,
             'per_page': '100',
             'page': 0,
         }
@@ -84,22 +45,50 @@ class HHParser(Parser):
         'User-Agent': 'api-test-agent'
     }
 
+    def clean_vacancy_data(self, vacancy):
+        """Cleans vacancy text """
+
+        vacancy_data = {
+            'name': vacancy['name'],
+            'area': vacancy['area']['name'],
+        }
+
+        if vacancy_data.get('salary'):
+            vacancy_data['salary_from'] = vacancy['salary'].get('from')
+            vacancy_data['salary_from'] = vacancy['s alary'].get('from')
+            vacancy_data['salary_currency'] = vacancy['salary']['currency']
+            vacancy_data['salary_gross'] = vacancy['salary']['gross']
+
+        vacancy_skill_set = set()
+
+        words = re.split(r'[;,"\(\)\{\}/\s]', str(vacancy))
+        words =[word.lower() for word in words]
+        for skill in self.KEY_SKILLS:
+            if skill in words:
+                vacancy_skill_set.add(skill)
+        vacancy_data['skill_set'] = vacancy_skill_set
+
+        return vacancy_data
+
     def parse(self):
+        """Parses vacancy text """
         result = {}
-        data = requests.get(self.URL, params=self.PARAMS, headers=self.HEADERS).json()
+        data = requests.get(self.URL, params=self.params, headers=self.HEADERS).json()
         current_page_vacancies = data['items']
         pages = data['pages']
-        for i in range(1): # change to pages
-            self.PARAMS['page'] = i
-            print(self.PARAMS['page'])
+        for i in range(1): # change range(x) to 'pages' to go for a full cycle
+            self.params['page'] = i
+            print(i)
             for vacancy in current_page_vacancies:
-                vac_data = requests.get(self.URL + '/' + vacancy['id'], params=self.PARAMS).json()
+                vac_data = requests.get(self.URL + '/' + vacancy['id'], params=self.params).json()
                 '''vac_id = vac_data['id']
                 vac_name = vac_data['name']
                 result[vac_id] = vac_name'''
 
                 vac_id = vac_data['id']
-                cleaned_data = Parser.clean_vacancy_data(vacancy)
+                # print(vac_id)
+                cleaned_data = HHParser.clean_vacancy_data(self, vacancy)
+                # print(cleaned_data)
                 result[vac_id] = cleaned_data
 
         return result
