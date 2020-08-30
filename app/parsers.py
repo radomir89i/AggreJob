f9e98b6d467c0ccb35b7324b9932543cfa8dab08
Radomir Vasilev <radomir89i@gmail.com>
2020-08-30 14:24:51 +0200

    csv writer added to parsers.py; parsed_data.csv added as example

--- app/parsers.py
+++ app/parsers.py
@@ -2,15 +2,18 @@ from abc import ABC, abstractmethod
 import requests
 import re
 import yaml
+import csv
 from bs4 import BeautifulSoup
+import lxml
+
 
 class Parser(ABC):
     @abstractmethod
-    def __init__(self, specialization):
+    def __init__(self, specialization: str):
         self.specialization = specialization
 
     # @abstractmethod
-    def get_config(self):
+    def get_config(self) -> list:
         """Get parameter from config.yml file"""
 
         with open('config.yml') as f:
@@ -18,19 +21,21 @@ class Parser(ABC):
         return config[self.specialization]
 
     @abstractmethod
-    def parse(self):
+    def parse(self) -> list:
         """Parses vacancy text """
         pass
 
     @abstractmethod
-    def clean_vacancy_data(self, vacancy):
+    def clean_vacancy_data(self, vacancy: dict) -> list:
         """Cleans vacancy text """
         pass
 
     @staticmethod
-    def write_csv_file(path, data):
+    def write_csv_file(path, data: list):
         """Writes parsing result to csv file"""
-        pass
+        with open(path, "w") as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerows(data)
 
 
 class HHParser(Parser):
@@ -50,33 +55,34 @@ class HHParser(Parser):
         }
 
     def clean_vacancy_data(self, vacancy):
+        vac_id = vacancy.get('id')
+        name = vacancy.get('name')
+        salary = vacancy.get('salary')
+        if salary:
+            salary_from = salary.get('from')
+            salary_to = salary.get('to')
+            currency = salary.get('currency')
+        else:
+            salary_from, salary_to, currency = None, None, None
+        area = vacancy['area']['name']
 
-        vacancy_data = {
-            'name': vacancy['name'],
-            'area': vacancy['area']['name'],
-        }
-
-        if vacancy_data.get('salary'):
-            vacancy_data['salary_from'] = vacancy['salary'].get('from')
-            vacancy_data['salary_from'] = vacancy['salary'].get('from')
-            vacancy_data['salary_currency'] = vacancy['salary']['currency']
-            vacancy_data['salary_gross'] = vacancy['salary']['gross']
-
-        vacancy_skill_set = set()
-
+        skill_set = []
         words = re.split(r'[;,"\(\)\{\}/\s]', str(vacancy))
-        words =[word.lower() for word in words]
+        words = [word.lower() for word in words]
         for skill in self.KEY_SKILLS:
             if skill in words:
-                vacancy_skill_set.add(skill)
-        vacancy_data['skill_set'] = vacancy_skill_set
-        vacancy_data['requirement'] = vacancy['snippet'].get('requirement')
-        vacancy_data['responsibility'] = vacancy['snippet'].get('responsibility')
+                skill_set.append(skill)
+
+        description = BeautifulSoup(vacancy.get('description'), 'lxml').text
+
+        vacancy_data = [vac_id, name, salary_from, salary_to, currency, area, skill_set, description]
+
         return vacancy_data
 
     def parse(self):
 
-        result = {}
+        result = []
+        result.append('id, name, salary_from, salary_to, currency, area, skill_set, description'.split(', '))
         data = requests.get(self.URL, params=self.params, headers=self.HEADERS).json()
         current_page_vacancies = data['items']
         pages = data['pages']
@@ -84,13 +90,9 @@ class HHParser(Parser):
             self.params['page'] = i
             print(i)
             for vacancy in current_page_vacancies:
-                '''for k, v in vacancy.items():
-                    print(k, v)'''
                 vac_data = requests.get(self.URL + '/' + vacancy['id'], params=self.params).json()
-                vac_id = vac_data['id']
-                cleaned_data = HHParser.clean_vacancy_data(self, vacancy)
-                result[vac_id] = cleaned_data
-
+                cleaned_data = HHParser.clean_vacancy_data(self, vac_data)
+                result.append(cleaned_data)
         return result
 
 
@@ -101,4 +103,5 @@ class MKParser:
 
 if __name__ == '__main__':
     parser = HHParser('python')
-    print(parser.parse())
+    Parser.write_csv_file('parsed_data.csv',parser.parse())
+
