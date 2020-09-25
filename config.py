cea235d1406c9da4500a745534ba9570d40af7fb
rostislav-g <rippastiy@gmail.com>
2020-09-25 21:47:45 +0300

    small fixes

--- config.py
+++ config.py
@@ -2,11 +2,24 @@ import os
 import yaml
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-POSTGRES = yaml.safe_load(open('config/db_credentials.yml'))
 
 
 class Config(object):
+    CONFIG_FOLDER = os.path.join(basedir, 'config')
+    VACANCY_FOLDER = os.path.join(basedir, 'vacancy_data')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'moscow-never-sleeps'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    with open(os.path.join(CONFIG_FOLDER, 'db_credentials.yaml')) as f:
+        PG_CREDENTIALS = yaml.load(f)
+
+        login = PG_CREDENTIALS['login']
+        password = PG_CREDENTIALS['password']
+        host = PG_CREDENTIALS['host']
+        port = PG_CREDENTIALS['port']
+        db = PG_CREDENTIALS['db']
+
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{host}:{port}@{login}:{password}/{db}'
+
+
+
