5e90d1e05a75c80257aa9ae944a65cd86aeefa55
radomir89i <radomir89i@mail.ru>
2020-09-25 18:22:17 +0200

    creds added for db

--- config.py
+++ config.py
@@ -1,20 +1,12 @@
 import os
-
+import yaml
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+POSTGRES = yaml.safe_load(open('config/db_credentials.yml'))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'moscow-never-sleeps'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    '''POSTGRES = {
-        'user': 'postgres',
-        'pw': 'password',
-        'db': 'my_database',
-        'host': 'localhost',
-        'port': '5432',
-    }'''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
