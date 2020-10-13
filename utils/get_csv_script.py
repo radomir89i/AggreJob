fc7330c04edb1dc8601ec598c1212631b567a551
radomir89i <radomir89i@mail.ru>
2020-10-13 19:08:05 +0200

    script added

---
+++ utils/get_csv_script.py
@@ -0,0 +1,49 @@
+from google.oauth2 import service_account
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+from googleapiclient.discovery import build
+import io
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SERVICE_ACCOUNT_FILE = '../credentials.json'
+FOLDER_ID = '1HrUEphX1chXTYCr7k2DsX6Q8viDxd2as'
+CREDENTIALS = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES
+)
+DB_CONN = 'top_secret_info'
+service = build('drive', 'v3', credentials=CREDENTIALS)
+
+result = service \
+    .files() \
+    .list(
+        pageSize=1000,
+        fields='nextPageToken, files(id, name, mimeType)') \
+    .execute()['files']
+
+
+def get_csv_files(path=os.getcwd()):
+    for file in result:
+        if file['mimeType'] == 'text/csv':
+            request = service.files().get_media(fileId=file['id'])
+            fh = io.FileIO(os.path.join(path, file['name']), 'wb')
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                print("Download %d%%." % int(status.progress() * 100))
+
+
+def load_from_csv_to_database(path=os.getcwd()):
+    engine = create_engine(DB_CONN)
+    files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.csv')]
+    for file in files:
+        data = pd.read_csv(file)
+        data.to_sql('vacancy', engine, if_exists='append', index=False)  # Not copying over the index
+
+
+if __name__ == '__main__':
+    get_csv_files()
+    load_from_csv_to_database()
