143138052a889a2810dfe8f27653c0e77dea1d67
radomir89i <radomir89i@mail.ru>
2020-10-15 11:14:49 +0200

    script modified

--- utils/get_csv_script.py
+++ utils/get_csv_script.py
@@ -25,6 +25,13 @@ result = service \
 
 
 def get_csv_files(path=os.getcwd()):
+
+    """
+    Gets all *.csv files from Google Drive folder and puts them in folder with given path (current folder by default).
+    :param path: path to folder where downloaded files should be placed
+    :return: None
+    """
+
     for file in result:
         if file['mimeType'] == 'text/csv':
             request = service.files().get_media(fileId=file['id'])
@@ -37,11 +44,18 @@ def get_csv_files(path=os.getcwd()):
 
 
 def load_from_csv_to_database(path=os.getcwd()):
+
+    """
+    Loads all *.csv files from given folder to database.
+    :param path: path to *csv files folder
+    :return: None
+    """
+
     engine = create_engine(DB_CONN)
     files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.csv')]
     for file in files:
         data = pd.read_csv(file)
-        data.to_sql('vacancy', engine, if_exists='append', index=False)  # Not copying over the index
+        data.to_sql('vacancy', engine, if_exists='append', index=False)
 
 
 if __name__ == '__main__':
